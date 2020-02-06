d2c277751b8fc21eb74cb5b39f88fb7564895780
ceyhunaziz <jeyhun.azizzade@pashabank.az>
2020-02-06 17:27:08 +0400

    Revert "List all owners on index page"
    This reverts commit 7cc554a3

--- src/main/java/guru/springframework/sfgpetclinic/controllers/OwnersController.java
+++ src/main/java/guru/springframework/sfgpetclinic/controllers/OwnersController.java
@@ -1,25 +1,14 @@
 package guru.springframework.sfgpetclinic.controllers;
 
-import guru.springframework.sfgpetclinic.services.OwnerService;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/owners")
 public class OwnersController {
 
-    private final OwnerService ownerService;
-
-    public OwnersController(OwnerService ownerService) {
-        this.ownerService = ownerService;
-    }
-
     @RequestMapping({"", "/", "/index", "/index.html"})
-    public String listOwners(Model model) {
-
-        model.addAttribute("owners",ownerService.findAll());
-
+    public String listOwners() {
         return "owners/index.html";
     }
 }
--- src/main/java/guru/springframework/sfgpetclinic/dao/OwnerRepository.java
+++ src/main/java/guru/springframework/sfgpetclinic/dao/OwnerRepository.java
@@ -4,9 +4,6 @@ import guru.springframework.sfgpetclinic.dao.model.Owner;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
 public interface OwnerRepository extends CrudRepository<Owner, Long> {
-    List<Owner> findAll();
 }
