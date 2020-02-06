123518a026affc99018af86f2fc826a807d8abec
ceyhunaziz <jeyhun.azizzade@pashabank.az>
2020-02-06 17:41:03 +0400

    show list of vets on vets index page. closes #14

--- src/main/java/guru/springframework/sfgpetclinic/dao/VetRepository.java
+++ src/main/java/guru/springframework/sfgpetclinic/dao/VetRepository.java
@@ -4,6 +4,11 @@ import guru.springframework.sfgpetclinic.dao.model.Vet;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface VetRepository extends CrudRepository<Vet, Long> {
+
+    List<Vet> findAll();
+
 }
--- src/main/java/guru/springframework/sfgpetclinic/services/VetService.java
+++ src/main/java/guru/springframework/sfgpetclinic/services/VetService.java
@@ -1,7 +1,20 @@
 package guru.springframework.sfgpetclinic.services;
 
+import guru.springframework.sfgpetclinic.dao.VetRepository;
+import guru.springframework.sfgpetclinic.dao.model.Vet;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class VetService {
+    private final VetRepository vetRepository;
+
+    public VetService(VetRepository vetRepository) {
+        this.vetRepository = vetRepository;
+    }
+
+    public List<Vet> findAll() {
+        return vetRepository.findAll();
+    }
 }
