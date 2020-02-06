7cc554a39a2adff4e0ea2b3a9c4f347f718b61a3
ceyhunaziz <jeyhun.azizzade@pashabank.az>
2020-02-06 17:25:58 +0400

    List all owners on index page

--- src/main/java/guru/springframework/sfgpetclinic/services/OwnerService.java
+++ src/main/java/guru/springframework/sfgpetclinic/services/OwnerService.java
@@ -1,7 +1,21 @@
 package guru.springframework.sfgpetclinic.services;
 
+import guru.springframework.sfgpetclinic.dao.OwnerRepository;
+import guru.springframework.sfgpetclinic.dao.model.Owner;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class OwnerService {
+    private final OwnerRepository ownerRepository;
+
+    public OwnerService(OwnerRepository ownerRepository) {
+        this.ownerRepository = ownerRepository;
+    }
+
+
+    public List<Owner> findAll() {
+        return ownerRepository.findAll();
+    }
 }
