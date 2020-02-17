4ae3730306345f6532e1c5d038ddfdde5ab35258
ceyhunaziz <jeyhun.azizzade@pashabank.az>
2020-02-17 16:53:32 +0400

    add properties to Owner. closes #25

--- src/main/java/guru/springframework/sfgpetclinic/dao/model/Owner.java
+++ src/main/java/guru/springframework/sfgpetclinic/dao/model/Owner.java
@@ -18,6 +18,9 @@ public class Owner implements Serializable {
     private String firstName;
     private String lastName;
 
+    private String address;
+    private String city;
+    private String telephone;
     Set<Pet> pets;
 
     public Owner() {
@@ -26,14 +29,6 @@ public class Owner implements Serializable {
     public Owner(String firstName, String lastName) {
         this.firstName = firstName;
         this.lastName = lastName;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
     }
 
     public String getFirstName() {
@@ -50,6 +45,30 @@ public class Owner implements Serializable {
 
     public void setLastName(String lastName) {
         this.lastName = lastName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
     }
 
     public Set<Pet> getPets() {
